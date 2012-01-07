36b6a7b3349f43913132ad1fbff75a9ae85fa11a
estebancacavelos <estebancacavelos@9c1b12bc-2c0a-58fe-6064-24b3b8bdd5e3>
2012-01-07 19:29:44 +0000

    Subida de archivos del proyecto en Visual stuido 2010 Express edition. Base de datos MS compact edition 3.5. C#

---
+++ Gimnasio/Window1.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Reflection;
+
+namespace Gimnasio
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Gimnasio.Database1Entities database1Entities = new Gimnasio.Database1Entities();
+            // Load data into clientes. You can modify this code as needed.
+            System.Windows.Data.CollectionViewSource clientesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("clientesViewSource")));
+            System.Data.Objects.ObjectQuery<Gimnasio.clientes> clientesQuery = this.GetclientesQuery(database1Entities);
+            clientesViewSource.Source = clientesQuery.Execute(System.Data.Objects.MergeOption.AppendOnly);
+        }
+
+        private System.Data.Objects.ObjectQuery<clientes> GetclientesQuery(Database1Entities database1Entities)
+        {
+            // Auto generated code
+
+            System.Data.Objects.ObjectQuery<Gimnasio.clientes> clientesQuery = database1Entities.clientes;
+            // Returns an ObjectQuery.
+            return clientesQuery;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void abrirVentanaClientes(object sender, RoutedEventArgs e)
+        {
+            // Get the current button.
+            Button cmd = (Button)e.OriginalSource;
+
+            // Create an instance of the window named
+            // by the current button.
+            Type type = this.GetType();
+            Assembly assembly = type.Assembly;
+            Window win = (Window)assembly.CreateInstance("Gimnasio.Window2");
+            // Show the window.
+            win.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // Get the current button.
+            Button cmd = (Button)e.OriginalSource;
+
+            // Create an instance of the window named
+            // by the current button.
+            Type type = this.GetType();
+            Assembly assembly = type.Assembly;
+            Window win = (Window)assembly.CreateInstance("Gimnasio.Window3");
+            //win.Owner = this;
+
+            // Show the window.
+            win.Show();
+                
+            System.Console.WriteLine("abrir la ventana de control");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            // Get the current button.
+            Button cmd = (Button)e.OriginalSource;
+
+            // Create an instance of the window named
+            // by the current button.
+            Type type = this.GetType();
+            Assembly assembly = type.Assembly;
+            Window win = (Window)assembly.CreateInstance("Gimnasio.Window4");
+            //win.Owner = this;
+
+            // BLOQUEA LAS OTRAS VENTANAS !
+            win.ShowDialog();
+                
+
+        }
+    }
+}
---
+++ Gimnasio/Window2.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gimnasio
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+
+        Gimnasio.Database1Entities database1Entities = new Gimnasio.Database1Entities();
+
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private System.Data.Objects.ObjectQuery<clientes> GetclientesQuery(Database1Entities database1Entities)
+        {
+            // Auto generated code
+
+            System.Data.Objects.ObjectQuery<Gimnasio.clientes> clientesQuery = database1Entities.clientes;
+            // Returns an ObjectQuery.
+            return clientesQuery;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            // Load data into clientes. You can modify this code as needed.
+            System.Windows.Data.CollectionViewSource clientesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("clientesViewSource")));
+            System.Data.Objects.ObjectQuery<Gimnasio.clientes> clientesQuery = this.GetclientesQuery(database1Entities);
+            clientesViewSource.Source = clientesQuery.Execute(System.Data.Objects.MergeOption.AppendOnly);
+            database1Entities.SaveChanges();
+        }
+
+        private void GuardarCambiosClientes(object sender, RoutedEventArgs e)
+        {
+            // advertir del cambio antes de cambiar. 
+            database1Entities.SaveChanges();
+           // Gimnasio.Database1Entities
+            // cuando se da click en el boton de guardar cambios, se tienen que guardar todos los objetos que fueron
+            //cambiados
+
+        }
+
+        private void c(object sender, MouseWheelEventArgs e)
+        {
+
+        }
+
+        private void clientesDataGrid_CurrentCellChanged(object sender, EventArgs e)
+        {
+            System.Console.WriteLine("hola");
+        }
+
+        private void clientesDataGrid_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+            System.Console.WriteLine("clientesDataGrid_SourceUpdated");
+        }
+
+        private void clientesDataGrid_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            System.Console.WriteLine("clientesDataGrid_IsEnabledChanged");
+        }
+
+        private void clientesDataGrid_IsHitTestVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            System.Console.WriteLine("clientesDataGrid_IsHitTestVisibleChanged");
+        }
+
+        private void clientesDataGrid_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            System.Console.WriteLine("clientesDataGrid_IsVisibleChanged");
+
+        }
+
+        private void clientesDataGrid_RowDetailsVisibilityChanged(object sender, DataGridRowDetailsEventArgs e)
+        {
+            System.Console.WriteLine("clientesDataGrid_RowDetailsVisibilityChanged");
+
+
+        }
+
+        private void clientesDataGrid_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            System.Console.WriteLine("clientesDataGrid_SelectedCellsChanged");
+        }
+
+        private void clientesDataGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            System.Console.WriteLine("clientesDataGrid_SizeChanged");
+        }
+
+        private void clientesDataGrid_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            button2.IsEnabled = true;
+            
+        }
+
+    }
+}
