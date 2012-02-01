9e264b87bfdeddabe4a786938ef89a26ffa77626
estebancacavelos@gmail.com <estebancacavelos@gmail.com@9c1b12bc-2c0a-58fe-6064-24b3b8bdd5e3>
2012-02-01 14:31:30 +0000

    Se agrego el sub menu Editar del menu Cuotas de la ventana principal. En la vista de la edicion de cuotas ya se mappea con la tabla en base de datos.

--- trunk/Gimnasio/VistaTiposCuotas.xaml.cs
+++ trunk/Gimnasio/VistaTiposCuotas.xaml.cs
@@ -22,5 +22,37 @@ namespace Gimnasio
         {
             InitializeComponent();
         }
+
+        private System.Data.Objects.ObjectQuery<clientes> GetclientesQuery(Database1Entities database1Entities)
+        {
+            // Auto generated code
+
+            System.Data.Objects.ObjectQuery<Gimnasio.clientes> clientesQuery = database1Entities.clientes;
+            // Returns an ObjectQuery.
+            return clientesQuery;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Gimnasio.Database1Entities database1Entities = new Gimnasio.Database1Entities();
+            // Load data into clientes. You can modify this code as needed.
+            System.Windows.Data.CollectionViewSource clientesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("clientesViewSource")));
+            System.Data.Objects.ObjectQuery<Gimnasio.clientes> clientesQuery = this.GetclientesQuery(database1Entities);
+            clientesViewSource.Source = clientesQuery.Execute(System.Data.Objects.MergeOption.AppendOnly);
+            // Load data into Cuotas. You can modify this code as needed.
+            System.Windows.Data.CollectionViewSource cuotasViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("cuotasViewSource")));
+            System.Data.Objects.ObjectQuery<Gimnasio.Cuotas> cuotasQuery = this.GetCuotasQuery(database1Entities);
+            cuotasViewSource.Source = cuotasQuery.Execute(System.Data.Objects.MergeOption.AppendOnly);
+        }
+
+        private System.Data.Objects.ObjectQuery<Cuotas> GetCuotasQuery(Database1Entities database1Entities)
+        {
+            // Auto generated code
+
+            System.Data.Objects.ObjectQuery<Gimnasio.Cuotas> cuotasQuery = database1Entities.Cuotas;
+            // Returns an ObjectQuery.
+            return cuotasQuery;
+        }
     }
 }
