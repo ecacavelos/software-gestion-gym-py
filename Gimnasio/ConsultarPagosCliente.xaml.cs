1ef1736ac394a28673adcae502689e13b2b30eff
estebancacavelos@gmail.com <estebancacavelos@gmail.com@9c1b12bc-2c0a-58fe-6064-24b3b8bdd5e3>
2012-02-02 14:05:09 +0000

    ConsultarPagosCliente: se codifico para que cuando se ingresa un nro. de cedula se traiga el cliente que corresponde y que imprima los campos Nombre, apellido y Fecha Vto. se modifico la base de datos para que no tenga null en la  fecha de vencimiento en la tabla pagos.

--- Gimnasio/ConsultarPagosCliente.xaml.cs
+++ Gimnasio/ConsultarPagosCliente.xaml.cs
@@ -18,6 +18,7 @@ namespace Gimnasio
     /// </summary>
     public partial class ConsultarPagosCliente : Window
     {
+        Gimnasio.Database1Entities database1Entities = new Gimnasio.Database1Entities();
         public ConsultarPagosCliente()
         {
             InitializeComponent();
@@ -38,11 +39,55 @@ namespace Gimnasio
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
-            Gimnasio.Database1Entities database1Entities = new Gimnasio.Database1Entities();
+            
             // Load data into Pagos. You can modify this code as needed.
             System.Windows.Data.CollectionViewSource pagosViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("pagosViewSource")));
             System.Data.Objects.ObjectQuery<Gimnasio.Pagos> pagosQuery = this.GetPagosQuery(database1Entities);
             pagosViewSource.Source = pagosQuery.Execute(System.Data.Objects.MergeOption.AppendOnly);
+        }
+        private void textBoxNroCedula_KeyDown(object sender, KeyEventArgs e)
+        {
+            // se presiono ENTER
+            if (e.Key.ToString() == "Return")
+            {
+
+
+                string esql = "select value c from clientes as c where c.nro_cedula= '" + this.textBoxNroCedula.Text + "\'";
+                var clientesVar = database1Entities.CreateQuery<clientes>(esql);
+
+                //Chequear si existe un cliente con ese nro. de Cedula. 
+                if (clientesVar.ToList().Count == 0)// NO HAY NADA. 
+                {
+                    // NO hacer nada. 
+                }
+                else //HAY ALGO. OJO: Puede que haya mas de un cliente, porque en la base de datos no esta el nro. de cedula como unico. 
+                {
+                    if (clientesVar.ToList().Count == 1)// Solo se selecciono un cliente, esta correcto
+                    {
+                        this.textBoxNombre.Text = clientesVar.ToArray()[0].nombre;
+                        this.textBoxApellido.Text = clientesVar.ToArray()[0].apellido;
+
+                        string fechaVencimientoQuery = "select value p from Pagos as p where p.fk_cliente=" + clientesVar.ToArray()[0].idCliente + " order by p.fecha_vencimiento desc limit 1";
+                        var fechaUltimoVencimientoResult = database1Entities.CreateQuery<Pagos>(fechaVencimientoQuery);
+
+                        if (fechaUltimoVencimientoResult.ToList().Count == 0)//NO TIENE PAGOS REALIZADOS
+                        {
+                            MessageBox.Show("Este cliente no tiene pagos realizados.");
+                        }
+                        else //TIENE PAGOS REALIZADOS.
+                        {
+                            //llenar el datagrid.
+                            this.textBlockFechaVto.Text = fechaUltimoVencimientoResult.ToArray()[0].fecha_vencimiento.Value.ToLongDateString();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("se presiono cualquier otra tecla que no es Return");
+
+            }
+
         }
     }
 }
