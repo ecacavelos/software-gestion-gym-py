b5361fe23736cb85db554d0776432247ed35ff28
estebancacavelos@gmail.com <estebancacavelos@gmail.com@9c1b12bc-2c0a-58fe-6064-24b3b8bdd5e3>
2012-02-07 15:25:28 +0000

    ConsultarPagosCliente.xaml: Se agregaron las columnas del datagrid para la fecha de pago y vencimiento de pagos de cada cliente. ConsultarPagosCliente.xaml.cs: Se introdujo toda la logica para que el usuario pueda borrar los pagos de los clientes.

--- trunk/Gimnasio/ConsultarPagosCliente.xaml.cs
+++ trunk/Gimnasio/ConsultarPagosCliente.xaml.cs
@@ -19,6 +19,11 @@ namespace Gimnasio
     public partial class ConsultarPagosCliente : Window
     {
         Gimnasio.Database1Entities database1Entities = new Gimnasio.Database1Entities();
+        Gimnasio.Database1Entities database1Entities2 = new Gimnasio.Database1Entities();
+
+        private System.Data.Objects.DataClasses.EntityCollection<Pagos> pagosCliente;
+        
+
         public ConsultarPagosCliente()
         {
             InitializeComponent();
@@ -50,8 +55,6 @@ namespace Gimnasio
             // se presiono ENTER
             if (e.Key.ToString() == "Return")
             {
-
-
                 string esql = "select value c from clientes as c where c.nro_cedula= '" + this.textBoxNroCedula.Text + "\'";
                 var clientesVar = database1Entities.CreateQuery<clientes>(esql);
 
@@ -66,28 +69,47 @@ namespace Gimnasio
                     {
                         this.textBoxNombre.Text = clientesVar.ToArray()[0].nombre;
                         this.textBoxApellido.Text = clientesVar.ToArray()[0].apellido;
-
-                        string fechaVencimientoQuery = "select value p from Pagos as p where p.fk_cliente=" + clientesVar.ToArray()[0].idCliente + " order by p.fecha_vencimiento desc limit 1";
-                        var fechaUltimoVencimientoResult = database1Entities.CreateQuery<Pagos>(fechaVencimientoQuery);
-
-                        if (fechaUltimoVencimientoResult.ToList().Count == 0)//NO TIENE PAGOS REALIZADOS
-                        {
-                            MessageBox.Show("Este cliente no tiene pagos realizados.");
-                        }
-                        else //TIENE PAGOS REALIZADOS.
-                        {
-                            //llenar el datagrid.
-                            this.textBlockFechaVto.Text = fechaUltimoVencimientoResult.ToArray()[0].fecha_vencimiento.Value.ToLongDateString();
-                        }
+                        this.pagosCliente = clientesVar.ToArray()[0].Pagos;
+                        this.dataGridPagos.ItemsSource = clientesVar.ToArray()[0].Pagos;
                     }
                 }
             }
             else
             {
                 System.Console.WriteLine("se presiono cualquier otra tecla que no es Return");
-
             }
 
+        }
+
+        private void unLoadingRow_Pagos(object sender, DataGridRowEventArgs e)
+        {
+            System.Windows.Forms.DialogResult result;
+            string esql = "select value c from clientes as c where c.nro_cedula= '" + this.textBoxNroCedula.Text + "\'";
+            var clientesVar = database1Entities2.CreateQuery<clientes>(esql);
+                
+
+            if (this.pagosCliente.Count < clientesVar.ToArray()[0].Pagos.Count) 
+            {
+
+                result = System.Windows.Forms.MessageBox.Show("Desea guardar los cambios efectuados?", "Confirmar modificaciones", System.Windows.Forms.MessageBoxButtons.YesNoCancel);
+
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    // ver los controles necesarios. 
+                    database1Entities.SaveChanges();
+                }
+                else
+                {
+                    if (result == System.Windows.Forms.DialogResult.No)
+                    {   // No hacer nada y poner de vuelta los elementos.
+                        database1Entities = new Gimnasio.Database1Entities();
+                        string esql2 = "select value c from clientes as c where c.nro_cedula= '" + this.textBoxNroCedula.Text + "\'";
+                        var clientesVar2 = database1Entities.CreateQuery<clientes>(esql2);
+                        this.pagosCliente = clientesVar2.ToArray()[0].Pagos;
+                        this.dataGridPagos.ItemsSource = clientesVar2.ToArray()[0].Pagos;
+                     }
+                }
+            }
         }
     }
 }
