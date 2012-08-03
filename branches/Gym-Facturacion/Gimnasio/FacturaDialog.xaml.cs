db83e552743af6b3bbe967d5738ef00f52d8ac26
david.eaj@gmail.com <david.eaj@gmail.com@9c1b12bc-2c0a-58fe-6064-24b3b8bdd5e3>
2012-08-03 15:20:50 +0000

    - Se implementó la función para que el usuario ingrese el número de factura a ser impresa. Se realizan las primeras validaciones sobre dicho número (no se permiten letras).

---
+++ branches/Gym-Facturacion/Gimnasio/FacturaDialog.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gimnasio
+{
+    /// <summary>
+    /// Interaction logic for FacturaDialog.xaml
+    /// </summary>
+    public partial class FacturaDialog : Window
+    {
+        public FacturaDialog()
+        {
+            InitializeComponent();
+            textBox1.Text = "0000001";
+        }
+
+        public string ResponseText
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+
+
+        private void buttonOK_Click(object sender, RoutedEventArgs e)
+        {
+            int temp = 0;
+            try
+            {
+                temp = Convert.ToInt32(textBox1.Text);
+            }
+            catch (Exception h)
+            {
+                MessageBox.Show("Por favor introduzca sólo números.");
+            }
+
+            if (temp > 0)
+            {
+                this.DialogResult = true;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
