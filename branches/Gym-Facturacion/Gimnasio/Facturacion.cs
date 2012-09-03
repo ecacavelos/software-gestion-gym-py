323cfc764cd085a4e7bed2e9ef097b71e9aef46c
david.eaj@gmail.com <david.eaj@gmail.com@9c1b12bc-2c0a-58fe-6064-24b3b8bdd5e3>
2012-09-03 14:44:56 +0000

    - Se agregó un checkbox para anular facturas en la vista de facturas. - Se permiten volver a imprimir facturas para pagos, si la factura anterior está anulada. (TODO: Verificar que siempre haya sólo una factura no anulada) - Se desactiva el botón de ingreso manual cuando no se encuentra el puerto paralelo.

--- branches/Gym-Facturacion/Gimnasio/Facturacion.cs
+++ branches/Gym-Facturacion/Gimnasio/Facturacion.cs
@@ -15,7 +15,6 @@ namespace Gimnasio
     public static class Facturacion
     {
 
-        //static int[] myMontosArray = new int[1];
         static int[] myIVAsExArray = new int[1];
         static int[] myIVAs05Array = new int[1];
         static int[] myIVAs10Array = new int[1];
@@ -43,85 +42,85 @@ namespace Gimnasio
                 var facturasVar = database1Entities.CreateQuery<Facturas>(esql);
 
                 // Si no se encontró este pago entre los facturados.
-                if (facturasVar.ToList().Count == 0)
+                /*if (facturasVar.ToList().Count == 0)
+                {*/
+
+                FacturaDialog dlg = new FacturaDialog(pago);
+                Nullable<bool> result = dlg.ShowDialog();
+
+                if (result == true)
                 {
-
-                    FacturaDialog dlg = new FacturaDialog(pago);
-                    Nullable<bool> result = dlg.ShowDialog();
-
-                    if (result == true)
-                    {
-                        // Se obtiene el número de factura ingresado en el cuadro de diálogo.
-                        current_factura.Nro_Factura = dlg.ResponseText_NroFactura;
-                    }
-                    else
-                    {
-                        return;
-                    }
-
-                    current_factura.Fecha_Emision = DateTime.Now;
-                    current_factura.Nombre_Pagador = pago.clientes.nombre + " " + pago.clientes.apellido;
-                    current_factura.RUC_Pagador = dlg.ResponseText_RUC;
-
-                    // Se determina el tipo de IVA y se acumula en el array correspondiente.
-                    myIVAsExArray[0] = 0;
-                    myIVAs05Array[0] = 0;
-                    myIVAs10Array[0] = 0;
-                    current_factura.Exentas_Total = 0;
-                    current_factura.IVA05_Total = 0;
-                    current_factura.IVA10_Total = 0;
-
-                    for (int i = 0; i < 1; i++)
-                    {
-                        int thisMonto = Convert.ToInt32(pago.Cuotas.monto);
-                        //myMontosArray[0] = thisMonto;
-                        if (pago.Cuotas.tipoIVA == "10%")
-                        {
-                            myIVAs10Array[i] = thisMonto;
-                        }
-                        else if (pago.Cuotas.tipoIVA == "5%")
-                        {
-                            myIVAs05Array[i] = thisMonto;
-                        }
-                        else if (pago.Cuotas.tipoIVA == "Exentas")
-                        {
-                            myIVAsExArray[i] = thisMonto;
-                        }
-
-                        current_factura.Exentas_Total += myIVAsExArray[i];
-                        current_factura.IVA05_Total += myIVAs05Array[i];
-                        current_factura.IVA10_Total += myIVAs10Array[i];
-                    }
-
-                    // Total a Pagar
-                    current_factura.Monto_Total = current_factura.Exentas_Total + current_factura.IVA05_Total +
-                        current_factura.IVA10_Total;
-                    // Liquidacion de IVA 5% y 10%
-                    current_factura.Liquidacion_IVA05 = current_factura.IVA05_Total / 21;
-                    current_factura.Liquidacion_IVA10 = current_factura.IVA10_Total / 11;
-                    current_factura.Liquidacion_IVA_Total = current_factura.Liquidacion_IVA05 + current_factura.Liquidacion_IVA10;
-
-                    current_factura.Concepto = "Pago cuota " + String.Format("{0:dd/MM/yyyy}", pago.fecha) +
-                        " a " + String.Format("{0:dd/MM/yyyy}", pago.fecha_vencimiento);
-
-                    // Se agrega la factura a la BD en la tabla "Facturas".
-                    database1Entities.Facturas.AddObject(current_factura);
-                    database1Entities.SaveChanges();
-
-                    // Se actualiza el campo "ya_facturado" de la tabla Pagos
-                    // para indicar que ese pago ya fue facturado.
-                    database1Entities.ExecuteStoreCommand(
-                        "UPDATE Pagos SET ya_facturado = 1 WHERE (Pagos.idPago = {0})", current_factura.fk_pago);
-                    database1Entities.SaveChanges();
-
-                    // Se llama al módulo de Impresión.
-                    Impresion.ImprimirFactura(current_factura, myIVAsExArray, myIVAs05Array, myIVAs10Array);
-
+                    // Se obtiene el número de factura ingresado en el cuadro de diálogo.
+                    current_factura.Nro_Factura = dlg.ResponseText_NroFactura;
                 }
                 else
                 {
-                    System.Console.WriteLine("No puede volver a facturar esto.");
+                    return;
                 }
+
+                current_factura.Fecha_Emision = DateTime.Now;
+                current_factura.Nombre_Pagador = pago.clientes.nombre + " " + pago.clientes.apellido;
+                current_factura.RUC_Pagador = dlg.ResponseText_RUC;
+
+                // Se determina el tipo de IVA y se acumula en el array correspondiente.
+                myIVAsExArray[0] = 0;
+                myIVAs05Array[0] = 0;
+                myIVAs10Array[0] = 0;
+                current_factura.Exentas_Total = 0;
+                current_factura.IVA05_Total = 0;
+                current_factura.IVA10_Total = 0;
+
+                for (int i = 0; i < 1; i++)
+                {
+                    int thisMonto = Convert.ToInt32(pago.Cuotas.monto);
+                    //myMontosArray[0] = thisMonto;
+                    if (pago.Cuotas.tipoIVA == "10%")
+                    {
+                        myIVAs10Array[i] = thisMonto;
+                    }
+                    else if (pago.Cuotas.tipoIVA == "5%")
+                    {
+                        myIVAs05Array[i] = thisMonto;
+                    }
+                    else if (pago.Cuotas.tipoIVA == "Exentas")
+                    {
+                        myIVAsExArray[i] = thisMonto;
+                    }
+
+                    current_factura.Exentas_Total += myIVAsExArray[i];
+                    current_factura.IVA05_Total += myIVAs05Array[i];
+                    current_factura.IVA10_Total += myIVAs10Array[i];
+                }
+
+                // Total a Pagar
+                current_factura.Monto_Total = current_factura.Exentas_Total + current_factura.IVA05_Total +
+                    current_factura.IVA10_Total;
+                // Liquidacion de IVA 5% y 10%
+                current_factura.Liquidacion_IVA05 = current_factura.IVA05_Total / 21;
+                current_factura.Liquidacion_IVA10 = current_factura.IVA10_Total / 11;
+                current_factura.Liquidacion_IVA_Total = current_factura.Liquidacion_IVA05 + current_factura.Liquidacion_IVA10;
+
+                current_factura.Concepto = "Pago cuota " + String.Format("{0:dd/MM/yyyy}", pago.fecha) +
+                    " a " + String.Format("{0:dd/MM/yyyy}", pago.fecha_vencimiento);
+
+                // Se agrega la factura a la BD en la tabla "Facturas".
+                database1Entities.Facturas.AddObject(current_factura);
+                database1Entities.SaveChanges();
+
+                // Se actualiza el campo "ya_facturado" de la tabla Pagos
+                // para indicar que ese pago ya fue facturado.
+                database1Entities.ExecuteStoreCommand(
+                    "UPDATE Pagos SET ya_facturado = 1 WHERE (Pagos.idPago = {0})", current_factura.fk_pago);
+                database1Entities.SaveChanges();
+
+                // Se llama al módulo de Impresión.
+                Impresion.ImprimirFactura(current_factura, myIVAsExArray, myIVAs05Array, myIVAs10Array);
+
+                /*}
+                else
+                {
+                    System.Console.WriteLine("No puede volver a facturar esto.");
+                }*/
 
             }
             else
